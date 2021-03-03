0d8a5a4f07485575dd259e508537b153cae5de6c
Daniel Elsner <daniel.elsner@tum.de>
2021-03-03 08:18:51 +0100

    Update pom and print

--- application_module_1/src/test/java/de/doubleslash/demo/coverage/module/one/FooTest.java
+++ application_module_1/src/test/java/de/doubleslash/demo/coverage/module/one/FooTest.java
@@ -7,8 +7,13 @@ import static org.junit.Assert.assertThat;
 
 public class FooTest {
 
+    private String getCurrentPid() {
+        return java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+    }
+
     @Test
     public void testFooMethod() {
+        System.out.println("STARTING SUITE FooTest PID: " + getCurrentPid());
         Foo testee = new Foo();
         assertThat(testee.fooMethod(), is(7));
     }
