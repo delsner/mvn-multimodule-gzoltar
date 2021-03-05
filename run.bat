4fe7bc986230ee84a6e847d7ab2123e335e68854
Daniel Elsner <daniel.elsner@tum.de>
2021-03-05 17:36:30 +0100

    Update bat

--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
-ping 127.0.0.1 -n 6 > nul
-mvn test -DfailIfNoTests=false -Dmaven.test.failure.ignore=true -DforkCount=1 -DreuseForks=false
+mvn clean test -pl application_module_4 -fn -DforkCount=1 -DreuseForks=false
